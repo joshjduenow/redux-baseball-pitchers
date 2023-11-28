84c19217fa1d242d87f6f3e0577c1799974a9eda
Joshua Duenow <joshjduenow@gmail.com>
2023-11-28 16:13:55 -0600

    loaded redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
