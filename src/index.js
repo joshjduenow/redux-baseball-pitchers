a643c2a2387d9f20579b8b766f3cd0f2c1ba5b4f
Joshua Duenow <joshjduenow@gmail.com>
2023-11-28 19:58:53 -0600

    created stores for pitchers and catchers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pitchers = (state = [], action) => {
+  if (action.type === "ADD_PITCHER") {
+    const newPitcher = action.payload;
+
+    return [...state, newPitcher];
+  }
+
+  return state;
+};
+const catchers = (state = [], action) => {
+    if (action.type === "ADD_CATCHER") {
+      const newCatcher = action.payload;
+  
+      return [...state, newCatcher];
+    }
+  
+    return state;
+  };
+
+const playerStore = createStore(
+  combineReducers({
+    pitchers,
+    catchers
+  })
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={playerStore}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
